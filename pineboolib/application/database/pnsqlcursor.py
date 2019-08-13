115533d5a614a1eb1b7f670f3f6193384f54cdeb
José A. Fernández <aullasistemas@gmail.com>
2019-08-13 10:09:49 +0200

    bugfix: al crear una factura , estando marcada otra e intentar introducir lineas , da error

--- pineboolib/application/database/pnsqlcursor.py
+++ pineboolib/application/database/pnsqlcursor.py
@@ -1907,8 +1907,7 @@ class PNSqlCursor(QtCore.QObject):
 
         self.d._currentregister = current.row()
         self.d._current_changed.emit(self.at())
-        # agregado para que FLTableDB actualice el buffer al pulsar.
-        self.refreshBuffer()
+
         self.d.doAcl()
         if self._action:
             logger.debug(
