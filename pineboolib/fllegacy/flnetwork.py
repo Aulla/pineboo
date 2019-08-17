e71ab6dfd1eb49731af097ea105c37eeb01a3814
José A. Fernández <aullasistemas@gmail.com>
2019-08-17 07:41:17 +0200

    tipeo y doc flnetwork

--- pineboolib/fllegacy/flnetwork.py
+++ pineboolib/fllegacy/flnetwork.py
@@ -1,73 +1,92 @@
+"""Flnetwork module."""
+
 # # -*- coding: utf-8 -*-
-from typing import Optional, cast
-from PyQt5 import QtCore  # type: ignore
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtNetwork import QNetworkRequest, QNetworkAccessManager, QNetworkReply  # type: ignore
+from PyQt5 import QtCore, QtNetwork
+
+from typing import Optional, cast, Any
+
+
 from pineboolib.core import decorators
 
 
 class FLNetwork(QtCore.QObject):
+    """FLNetwork class."""
 
-    url = None
-    request = None
-    manager = None
+    url: str
+    request: QtNetwork.QNetworkRequest
+    manager: QtNetwork.QNetworkAccessManager
 
-    reply: Optional[QNetworkReply] = None
+    reply: Optional[QtNetwork.QNetworkReply] = None
 
     finished = QtCore.pyqtSignal()
     start = QtCore.pyqtSignal()
     data = QtCore.pyqtSignal(str)
     dataTransferProgress = QtCore.pyqtSignal(int, int)
 
-    def __init__(self, url) -> None:
+    def __init__(self, url: str) -> None:
+        """Inicialize."""
+
         super(FLNetwork, self).__init__()
         self.url = url
 
-        self.request = QNetworkRequest()
+        self.request = QtNetwork.QNetworkRequest()
 
-        self.manager = QNetworkAccessManager()
+        self.manager = QtNetwork.QNetworkAccessManager()
         # self.manager.readyRead.connect(self._slotNetworkStart)
-        finished_signal = cast(pyqtSignal, self.manager.finished)
-        finished_signal.connect(self._slotNetworkFinished)
+        cast(QtCore.pyqtSignal, self.manager.finished).connect(self._slotNetworkFinished)
         # finished_signal["QNetworkReply*"].connect(self._slotNetworkFinished) # FIXME: What does this code?
         # self.data.connect(self._slotNetWorkData)
         # self.dataTransferProgress.connect(self._slotNetworkProgress)
 
     @decorators.BetaImplementation
-    def get(self, location):
+    def get(self, location: str) -> None:
+        """Get value from a location."""
+
         self.request.setUrl(QtCore.QUrl("%s%s" % (self.url, location)))
         self.reply = self.manager.get(self.request)
         try:
-            self.reply.uploadProgress.disconnect(self._slotNetworkProgress)
-            self.reply.downloadProgress.disconnect(self._slotNetworkProgress)
+            cast(QtCore.pyqtSignal, self.reply.uploadProgress).disconnect(self._slotNetworkProgress)
+            cast(QtCore.pyqtSignal, self.reply.downloadProgress).disconnect(
+                self._slotNetworkProgress
+            )
         except Exception:
             pass
 
-        self.reply.downloadProgress.connect(self._slotNetworkProgress)
+        cast(QtCore.pyqtSignal, self.reply.downloadProgress).connect(self._slotNetworkProgress)
 
     @decorators.BetaImplementation
-    def put(self, data, location):
+    def put(self, data: Any, location: str) -> None:
+        """Send data to a location."""
+
         self.request.setUrl(QtCore.QUrl("%s%s" % (self.url, location)))
         self.reply = self.manager.put(data, self.request)
         try:
-            self.reply.uploadProgress.disconnect(self._slotNetworkProgress)
-            self.reply.downloadProgress.disconnect(self._slotNetworkProgress)
+            cast(QtCore.pyqtSignal, self.reply.uploadProgress).disconnect(self._slotNetworkProgress)
+            cast(QtCore.pyqtSignal, self.reply.downloadProgress).disconnect(
+                self._slotNetworkProgress
+            )
         except Exception:
             pass
-        self.uploadProgress.connect(self.slotNetworkProgress)
+        cast(QtCore.pyqtSignal, self.uploadProgress).connect(self.slotNetworkProgress)
 
     @decorators.BetaImplementation
-    def copy(self, fromLocation, toLocation):
-        self.request.setUrl("%s%s" % (self.url, fromLocation))
+    def copy(self, fromLocation: str, toLocation: str) -> None:
+        """Copy data from a location to another."""
+
+        self.request.setUrl(QtCore.QUrl("%s%s" % (self.url, fromLocation)))
         data = self.manager.get(self.request)
         self.put(data.readAll(), toLocation)
 
     @decorators.pyqtSlot()
-    def _slotNetworkStart(self):
+    def _slotNetworkStart(self) -> None:
+        """Emit start signal."""
+
         self.start.emit()
 
     @decorators.pyqtSlot()
-    def _slotNetworkFinished(self, reply=None):
+    def _slotNetworkFinished(self, reply: Any = None) -> None:
+        """Emit finished signal."""
+
         self.finished.emit()
 
     # @decorators.pyqtSlot(QtCore.QByteArray)
@@ -75,7 +94,9 @@ class FLNetwork(QtCore.QObject):
     #    buffer = b
     #    self.data.emit(b)
 
-    def _slotNetworkProgress(self, bDone, bTotal) -> None:
+    def _slotNetworkProgress(self, bDone: int, bTotal: int) -> None:
+        """Process data received."""
+
         if self.reply is None:
             raise Exception("No reply in progress")
         self.dataTransferProgress.emit(bDone, bTotal)
