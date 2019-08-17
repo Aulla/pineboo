1c08773b52fbf05088edd8e3684a6d77d951f2a2
José A. Fernández <aullasistemas@gmail.com>
2019-08-17 09:12:45 +0200

    tipeo y doc flspinbox.

--- pineboolib/fllegacy/flspinbox.py
+++ pineboolib/fllegacy/flspinbox.py
@@ -1,20 +1,34 @@
+"""Flspinbox module."""
 # -*- coding: utf-8 -*-
-from typing import Any
-from pineboolib.qt3_widgets.qspinbox import QSpinBox
+
+from pineboolib.qt3_widgets import qspinbox
+
+from typing import Any, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from PyQt5 import QtWidgets  # noqa: F401
 
 
-class FLSpinBox(QSpinBox):
-    def __init__(self, parent=None) -> None:
+class FLSpinBox(qspinbox.QSpinBox):
+    """FLSpinBox class."""
+
+    def __init__(self, parent: Optional["QtWidgets.QWidget"] = None) -> None:
+        """Inicialize."""
         super().__init__(parent)
         # editor()setAlignment(Qt::AlignRight);
 
-    def setMaxValue(self, v) -> None:
+    def setMaxValue(self, v: int) -> None:
+        """Set maximum value."""
+
         self.setMaximum(v)
 
-    def getValue(self) -> Any:
+    def getValue(self) -> int:
+        """Return actual value."""
+
         return super().value()
 
-    def setValue(self, val) -> None:
+    def setValue(self, val: int) -> None:
+        """Set a value."""
         super().setValue(val)
 
     value: Any = property(getValue, setValue)  # type: ignore
