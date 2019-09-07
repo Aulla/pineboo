546cfa89791c91d209774ff5a44ac03c35d754e6
José A. Fernández <aullasistemas@gmail.com>
2019-09-07 19:04:01 +0200

    test qt3ui_parser

---
+++ pineboolib/application/parsers/qt3uiparser/test_qt3ui.py
@@ -0,0 +1,20 @@
+"""Test_pnsqlquery module."""
+
+import unittest
+from pineboolib.loader.main import init_testing
+from pineboolib import application
+
+
+class TestQT3UIParser(unittest.TestCase):
+    """TestQT3UIParser Class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Ensure pineboo is initialized for testing."""
+        init_testing()
+
+    def test_mainForm(self) -> None:
+        """Test mainForm widget."""
+        act_sys = application.project.actions["sys"]
+        self.assertTrue(act_sys)
+        act_sys.load()
