e599cd0bd4c3c7897978a2b479d74d321fd43f03
José A. Fernández <aullasistemas@gmail.com>
2019-09-15 18:07:39 +0200

    test acls tables globales

--- pineboolib/application/acls/tests/test_acls.py
+++ pineboolib/application/acls/tests/test_acls.py
@@ -36,7 +36,15 @@ class TestACLS(unittest.TestCase):
         cursor_flacls.setActivatedCheckIntegrity(False)
         cursor_flacls.refreshBuffer()
         cursor_flacls.setValueBuffer("idacl", "primera")
-        cursor_flacls.setValueBuffer("descripcion", "firsrt acl")
+        cursor_flacls.setValueBuffer("descripcion", "first acl")
+        cursor_flacls.setValueBuffer("prioridadgrupointro", 2)
+        cursor_flacls.setValueBuffer("prioridadusuariointro", 1)
+        cursor_flacls.commitBuffer()
+        cursor_flacls.setModeAccess(cursor_flacls.Insert)
+        cursor_flacls.setActivatedCheckIntegrity(False)
+        cursor_flacls.refreshBuffer()
+        cursor_flacls.setValueBuffer("idacl", "segunda")
+        cursor_flacls.setValueBuffer("descripcion", "second acl")
         cursor_flacls.setValueBuffer("prioridadgrupointro", 2)
         cursor_flacls.setValueBuffer("prioridadusuariointro", 1)
         cursor_flacls.commitBuffer()
@@ -74,30 +82,84 @@ class TestACLS(unittest.TestCase):
         cursor_flacs.commitBuffer()
         id_acs_2 = cursor_flacs.valueBuffer("idac")
 
+        # global tables '--'
+        cursor_flacs.setModeAccess(cursor_flacs.Insert)
+        cursor_flacs.setActivatedCheckIntegrity(False)
+        cursor_flacs.refreshBuffer()
+        cursor_flacs.setValueBuffer("prioridad", 3)
+        cursor_flacs.setValueBuffer("tipo", "table")
+        cursor_flacs.setValueBuffer("nombre", "flareas")
+        cursor_flacs.setValueBuffer("idgroup", "usuarios")
+        cursor_flacs.setValueBuffer("idacl", "segunda")
+        cursor_flacs.setValueBuffer("descripcion", "Sistema:Administración:table:Maestro:flareas")
+        cursor_flacs.setValueBuffer("degrupo", True)
+        cursor_flacs.setValueBuffer("idarea", "sys")
+        cursor_flacs.setValueBuffer("idmodule", "sys")
+        cursor_flacs.setValueBuffer("tipoform", "Maestro")
+        cursor_flacs.setValueBuffer("permiso", "--")
+        cursor_flacs.commitBuffer()
+
+        # global tables 'r-'
+        cursor_flacs.setModeAccess(cursor_flacs.Insert)
+        cursor_flacs.setActivatedCheckIntegrity(False)
+        cursor_flacs.refreshBuffer()
+        cursor_flacs.setValueBuffer("prioridad", 4)
+        cursor_flacs.setValueBuffer("tipo", "table")
+        cursor_flacs.setValueBuffer("nombre", "flusers")
+        cursor_flacs.setValueBuffer("idgroup", "usuarios")
+        cursor_flacs.setValueBuffer("idacl", "segunda")
+        cursor_flacs.setValueBuffer("descripcion", "Sistema:Administración:table:Maestro:flusers")
+        cursor_flacs.setValueBuffer("degrupo", True)
+        cursor_flacs.setValueBuffer("idarea", "sys")
+        cursor_flacs.setValueBuffer("idmodule", "sys")
+        cursor_flacs.setValueBuffer("tipoform", "Maestro")
+        cursor_flacs.setValueBuffer("permiso", "r-")
+        cursor_flacs.commitBuffer()
+
+        # global tables 'rw'
+        cursor_flacs.setModeAccess(cursor_flacs.Insert)
+        cursor_flacs.setActivatedCheckIntegrity(False)
+        cursor_flacs.refreshBuffer()
+        cursor_flacs.setValueBuffer("prioridad", 5)
+        cursor_flacs.setValueBuffer("tipo", "table")
+        cursor_flacs.setValueBuffer("nombre", "fltest")
+        cursor_flacs.setValueBuffer("idgroup", "usuarios")
+        cursor_flacs.setValueBuffer("idacl", "segunda")
+        cursor_flacs.setValueBuffer("descripcion", "Sistema:Administración:table:Maestro:fltest")
+        cursor_flacs.setValueBuffer("degrupo", True)
+        cursor_flacs.setValueBuffer("idarea", "sys")
+        cursor_flacs.setValueBuffer("idmodule", "sys")
+        cursor_flacs.setValueBuffer("tipoform", "Maestro")
+        cursor_flacs.setValueBuffer("permiso", "rw")
+        cursor_flacs.commitBuffer()
+
         cursor_flacos = pnsqlcursor.PNSqlCursor("flacos")
         cursor_flacos.setModeAccess(cursor_flacos.Insert)
         cursor_flacos.refreshBuffer()
-        cursor_flacos.setValueBuffer("nombre", "descripcion")  # field_name
-        cursor_flacos.setValueBuffer("permiso", "r-")  # No visible
+        # single 'r-'
+        cursor_flacos.setValueBuffer("nombre", "descripcion")
+        cursor_flacos.setValueBuffer("permiso", "r-")
         cursor_flacos.setValueBuffer("idac", id_acs_1)
         cursor_flacos.setValueBuffer("tipocontrol", "Tabla")
         cursor_flacos.commitBuffer()
         cursor_flacos.setModeAccess(cursor_flacos.Insert)
         cursor_flacos.refreshBuffer()
-        cursor_flacos.setValueBuffer("nombre", "idgroup")  # field_name
-        cursor_flacos.setValueBuffer("permiso", "--")  # No visible
+        # single '--'
+        cursor_flacos.setValueBuffer("nombre", "idgroup")
+        cursor_flacos.setValueBuffer("permiso", "--")
         cursor_flacos.setValueBuffer("idac", id_acs_1)
         cursor_flacos.setValueBuffer("tipocontrol", "Tabla")
         cursor_flacos.commitBuffer()
         cursor_flacos.setModeAccess(cursor_flacos.Insert)
         cursor_flacos.refreshBuffer()
-        cursor_flacos.setValueBuffer("nombre", "descripcion")  # field_name
-        cursor_flacos.setValueBuffer("permiso", "rw")  # No visible
+        # single 'rw'
+        cursor_flacos.setValueBuffer("nombre", "descripcion")
+        cursor_flacos.setValueBuffer("permiso", "rw")
         cursor_flacos.setValueBuffer("idac", id_acs_2)
         cursor_flacos.commitBuffer()
 
     def test_tables_flacos(self) -> None:
-        """Test table acls from flacos."""
+        """Test table acls from flacos especifics."""
 
         sys_type = systype.SysType()
         sys_type.installACL("primera")
@@ -126,6 +188,40 @@ class TestACLS(unittest.TestCase):
         # descripcion = 'rw'
         self.assertTrue(field_descripcion.editable())
         self.assertTrue(field_descripcion.visible())
+
+    def test_tables_globals(self) -> None:
+        """Test table acls globals."""
+
+        sys_type = systype.SysType()
+        sys_type.installACL("segunda")
+        acl = pnaccesscontrollists.PNAccessControlLists()
+        acl.init()
+        flapplication.aqApp.set_acl(acl)
+
+        # clear metadata cache
+        flapplication.aqApp.db().manager().cacheMetaDataSys_ = {}
+        flapplication.aqApp.db().manager().cacheMetaData_ = {}
+
+        mtd_flareas = flapplication.aqApp.db().manager().metadata("flareas")
+        self.assertTrue(mtd_flareas)
+        # '--'
+        field_descripcion = mtd_flareas.field("descripcion")
+        self.assertFalse(field_descripcion.editable())
+        self.assertFalse(field_descripcion.visible())
+
+        mtd_flusers = flapplication.aqApp.db().manager().metadata("flusers")
+        self.assertTrue(mtd_flusers)
+        # 'r-'
+        field_descripcion = mtd_flusers.field("descripcion")
+        self.assertFalse(field_descripcion.editable())
+        self.assertTrue(field_descripcion.visible())
+
+        mtd_fltest = flapplication.aqApp.db().manager().metadata("fltest")
+        self.assertTrue(mtd_fltest)
+        # 'rw'
+        field = mtd_fltest.field("date_field")
+        self.assertTrue(field.editable())
+        self.assertTrue(field.visible())
 
     @classmethod
     def tearDownClass(cls) -> None:
