890ce8c5a7ad63a656ed27ba311b83bec4297d78
José A. Fernández <aullasistemas@gmail.com>
2019-10-08 07:56:36 +0200

    test translations

---
+++ pineboolib/fllegacy/tests/test_translations.py
@@ -0,0 +1,18 @@
+"""Test_translations module."""
+
+import unittest
+
+
+class TestTranslations(unittest.TestCase):
+    def test_full(self) -> None:
+        from pineboolib.qsa import qsa
+
+        qsa.aqApp.loadTranslationFromModule("sys", "es")
+
+        util = qsa.FLUtil()
+        self.assertEqual(util.translate("scripts", "single"), "simple!")
+        self.assertEqual(util.translate("scripts", "variable %s") % "uno", "con variable uno!")
+        self.assertEqual(
+            util.translate("scripts", "multiple variable %s , %s") % ("uno", "dos"),
+            "Test uno y dos",
+        )
