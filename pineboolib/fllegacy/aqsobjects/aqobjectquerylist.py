b094123d86a69c8d8c7d179234ce3258a12e685e
José A. Fernández <aullasistemas@gmail.com>
2019-11-13 12:40:26 +0100

    normalizado

--- pineboolib/fllegacy/aqsobjects/aqobjectquerylist.py
+++ pineboolib/fllegacy/aqsobjects/aqobjectquerylist.py
@@ -3,7 +3,7 @@
 from PyQt5 import QtCore, QtWidgets
 
 from pineboolib.application import types
-from typing import List, Union, Optional
+from typing import Union, Optional
 
 
 def AQObjectQueryList(
