749edf6ae87b2a8e8b1825751257c4050233885c
José A. Fernández <aullasistemas@gmail.com>
2019-11-30 10:36:18 +0100

    fixç

--- pineboolib/application/types.py
+++ pineboolib/application/types.py
@@ -793,10 +793,7 @@ class FileStatic(FileBaseClass):
         with codecs.open(file_, "r" if not bytes else "rb", encoding="ISO-8859-15") as f:
             ret = f.read()
 
-        if bytes:
-            return ret.decode("UTF-8")
-        else:
-            return ret
+        return ret
 
     @staticmethod
     def write(file_: str, data: Union[str, bytes], length: int = -1) -> None:
