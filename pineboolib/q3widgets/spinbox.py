a3a3197c6563b8fa4fb319a4050156ff306c8c18
José A. Fernández <aullasistemas@gmail.com>
2019-12-06 10:38:58 +0100

    añadidas propiedades a SpinBox

--- pineboolib/q3widgets/spinbox.py
+++ pineboolib/q3widgets/spinbox.py
@@ -5,4 +5,30 @@ from . import qspinbox
 class SpinBox(qspinbox.QSpinBox):
     """SpinBox class."""
 
-    pass
+    def getMax(self) -> int:
+        """Return Maximum."""
+        return super().maximum()
+
+    def setMax(self, max: int) -> None:
+        """Set Maximum."""
+        super().setMaximum(max)
+
+    def getMin(self) -> int:
+        """Return Minimum."""
+        return super().minimum()
+
+    def setMin(self, min: int) -> None:
+        """Set Minimum."""
+        super().setMinimum(min)
+
+    def getValue(self) -> int:
+        """Return value."""
+        return super().value()
+
+    def setValue(self, value: int) -> None:
+        """Set Minimum."""
+        super().setValue(value)
+
+    maximum: int = property(getMax, setMax)  # type: ignore [assignment] # noqa: F821
+    minimum: int = property(getMin, setMin)  # type: ignore [assignment] # noqa: F821
+    value: int = property(getValue, setValue)  # type: ignore [assignment] # noqa: F821
