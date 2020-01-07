a8da673bd03a4f12b5ea4910d3c3b3055ad9a74a
José A. Fernández <aullasistemas@gmail.com>
2020-01-07 10:17:43 +0100

    la documentación recoge la versión de pineboolib

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 from pineboolib.loader.main import init_cli  # noqa: F402
+from pineboolib import application
+application.project.load_version()
 
 init_cli()
 # -- Project information -----------------------------------------------------
@@ -26,7 +28,7 @@ copyright = "2019, David Martínez Martí, Jose Antonio Fernández Fernández an
 author = "David Martínez Martí, Jose Antonio Fernández Fernández and others"
 
 # The full version, including alpha/beta/rc tags
-release = "0.12"
+release = application.project.version.split(" ")[1]
 
 
 # -- General configuration ---------------------------------------------------
