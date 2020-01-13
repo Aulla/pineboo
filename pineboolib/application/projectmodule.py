84a1ed05fd25c1a73eaa2bd3b260a8ed95cf379a
José A. Fernández <aullasistemas@gmail.com>
2020-01-13 10:50:31 +0100

    0.67.15

--- pineboolib/application/projectmodule.py
+++ pineboolib/application/projectmodule.py
@@ -584,7 +584,7 @@ class Project(object):
 
     def load_version(self):
         """Initialize current version numbers."""
-        self.version = "0.67.14"
+        self.version = "0.67.15"
         if settings.config.value("application/dbadmin_enabled", False):
             self.version = "DBAdmin v%s" % self.version
         else:
