1d22275acc28c21efabcecdff851c637cf3c60e9
José A. Fernández <aullasistemas@gmail.com>
2020-01-29 11:42:30 +0100

    avances compilancion ios

--- deploy_tools/build_binaries_ios.sh
+++ deploy_tools/build_binaries_ios.sh
@@ -34,7 +34,7 @@ tar -xvzf $FILE_SQLLITE.tar.gz -C ./sqlite3 > /dev/null
 cd $SRC_DIR_SQLLITE
 cd $FILE_SQLLITE
 #./configure --host=arm-apple-darwin --enable-static --enable-dynamic-extensions
-./configure --host=arm-apple-darwin CXXFLAGS="—Os -DSQLITE_ENABLE_UNLOCK_NOTIFY=1 -arch arm64 -std=c++14 -stdlib=libc++" CC="clang" CXX="clang++" LD="ld" AR="ar" --disable-shared --enable-static --enable-dynamic-extensions
+./configure CXXFLAGS="—Os -DSQLITE_ENABLE_UNLOCK_NOTIFY=1 -arch arm64 -std=c++14 -stdlib=libc++" CC="clang" CXX="clang++" LD="ld" AR="ar" --disable-shared --enable-static --enable-dynamic-extensions
 
 make
 cd ..
