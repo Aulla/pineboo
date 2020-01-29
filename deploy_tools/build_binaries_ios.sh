4529757e62f13c77c5517657a93b98b89cfbac3b
José A. Fernández <aullasistemas@gmail.com>
2020-01-29 11:09:29 +0100

    avances compilancion ios

--- deploy_tools/build_binaries_ios.sh
+++ deploy_tools/build_binaries_ios.sh
@@ -2,7 +2,10 @@
 TARGET="ios"
 SYSROOT=""
 FILE_SQLLITE="sqlite-autoconf-3280000"
+: ${IPHONE_SDKVERSION:=`xcodebuild -showsdks | grep iphoneos | egrep "[[:digit:]]+\.[[:digit:]]+" -o | tail -1`}
+: ${XCODE_ROOT:=`xcode-select -print-path`}
 SRC_DIR_SQLLITE="sqlite3"
+export CFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot $XCODE_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IPHONE_SDKVERSION}.sdk -mios-version-min=${IPHONE_SDKVERSION}"
 
 if [ -e "sysroots/$TARGET" ]; then
 SYSROOT="--no-sysroot"
