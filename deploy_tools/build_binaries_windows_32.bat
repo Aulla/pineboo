22735a9f3541854c41afbd9ef3a0fc484cc6876d
José A. Fernández <aullasistemas@gmail.com>
2020-01-30 17:27:39 +0100

    avances deploy windows

--- deploy_tools/build_binaries_windows_32.bat
+++ deploy_tools/build_binaries_windows_32.bat
@@ -22,7 +22,7 @@ md sysroots\extra_libs\%TARGET%
 cd sysroots\extra_libs\%TARGET%
 md sqlite3
 md sqlite3\%SQLITEVER%
-cp src\%SQLITEVER%\* sqlite3\%SQLITEVER% /E
+xcopy src\%SQLITEVER%\* sqlite3\%SQLITEVER% /E
 
 cd sqlite3\%SQLITEVER%
 cl sqlite3.c -link -dll -out:sqlite3.dll
