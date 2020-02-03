aad3974db43a745f183e2c2b53fd6290fbeec257
José A. Fernández <aullasistemas@gmail.com>
2020-02-03 09:01:45 +0100

    dateAMDtoDMA y dateDMAtoAMD retorns siempre str(2)

--- pineboolib/application/utils/date_conversion.py
+++ pineboolib/application/utils/date_conversion.py
@@ -8,30 +8,30 @@ from PyQt5.QtCore import QDate  # type: ignore
 from typing import Optional, List, Any
 
 
-def date_dma_to_amd(f: str) -> Optional[str]:
+def date_dma_to_amd(date_str: str) -> Optional[str]:
     """
     Convert day, month, year to year, month day.
 
-    @param f: date string.
+    @param date_str: date string.
     @return date string formated.
     """
 
-    if not f:
+    if not date_str:
         return None
 
-    f = str(f)
-    if f.find("T") > -1:
-        f = f[: f.find("T")]
+    date_str = str(date_str)
+    if date_str.find("T") > -1:
+        date_str = date_str[: date_str.find("T")]
 
     array_: List[str] = []
     dia_ = None
     mes_ = None
     ano_ = None
 
-    if f.find("-") > -1:
-        array_ = f.split("-")
-    elif f.find("/") > -1:
-        array_ = f.split("/")
+    if date_str.find("-") > -1:
+        array_ = date_str.split("-")
+    elif date_str.find("/") > -1:
+        array_ = date_str.split("/")
 
     if array_:
         if len(array_) == 3:
@@ -39,36 +39,36 @@ def date_dma_to_amd(f: str) -> Optional[str]:
             mes_ = array_[1]
             ano_ = array_[2]
         else:
-            dia_ = f[0:2]
-            mes_ = f[2:2]
-            ano_ = f[4:4]
+            dia_ = date_str[0:2]
+            mes_ = date_str[2:2]
+            ano_ = date_str[4:4]
 
-    return "%s-%s-%s" % (ano_, mes_, dia_)
+    return "%s-%s-%s" % (ano_, mes_, dia_) if ano_ else ""
 
 
-def date_amd_to_dma(f: str) -> Optional[str]:
+def date_amd_to_dma(date_str: str) -> Optional[str]:
     """
     Convert year, month day to day, month, year.
 
-    @param f: date string.
+    @param date_str: date string.
     @return date string formated.
     """
 
-    if not f:
+    if not date_str:
         return None
 
-    f = str(f)
-    if f.find("T") > -1:
-        f = f[: f.find("T")]
+    date_str = str(date_str)
+    if date_str.find("T") > -1:
+        date_str = date_str[: date_str.find("T")]
 
     array_: List[str] = []
     dia_ = None
     mes_ = None
     ano_ = None
-    if f.find("-") > -1:
-        array_ = f.split("-")
-    elif f.find("/") > -1:
-        array_ = f.split("/")
+    if date_str.find("-") > -1:
+        array_ = date_str.split("-")
+    elif date_str.find("/") > -1:
+        array_ = date_str.split("/")
 
     if array_:
         if len(array_) == 3:
@@ -76,11 +76,11 @@ def date_amd_to_dma(f: str) -> Optional[str]:
             mes_ = array_[1]
             dia_ = array_[2]
         else:
-            ano_ = f[0:4]
-            mes_ = f[4:2]
-            dia_ = f[6:2]
+            ano_ = date_str[0:4]
+            mes_ = date_str[4:2]
+            dia_ = date_str[6:2]
 
-    return "%s-%s-%s" % (dia_, mes_, ano_)
+    return "%s-%s-%s" % (dia_, mes_, ano_) if ano_ else ""
 
 
 def convert_to_qdate(date: Any) -> QDate:
