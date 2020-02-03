764a45e46da176e34073c7db8c81bf81c5b06279
José A. Fernández <aullasistemas@gmail.com>
2020-02-03 09:07:02 +0100

    QTextStream.read()

--- pineboolib/q3widgets/qtextstream.py
+++ pineboolib/q3widgets/qtextstream.py
@@ -2,6 +2,8 @@
 
 from PyQt5 import QtCore
 
+from typing import Optional
+
 
 class QTextStream(QtCore.QTextStream):
     """QTextStream class."""
@@ -9,3 +11,11 @@ class QTextStream(QtCore.QTextStream):
     def opIn(self, text_):
         """Set value to QTextStream."""
         self.device().write(text_.encode())
+
+    def read(self, max_len: Optional[int] = 0) -> str:
+        """Read datas from QTextStream."""
+
+        if max_len > 0:
+            return super().read(max_len)
+        else:
+            return super().readAll()
