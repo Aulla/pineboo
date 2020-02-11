2ca2278aaf30be68a44788ad62a7f0d6b4b2aa88
José A. Fernández <aullasistemas@gmail.com>
2020-02-11 11:18:29 +0100

    normalizado

--- pineboolib/fllegacy/aqsobjects/aqsbuttongroup.py
+++ pineboolib/fllegacy/aqsobjects/aqsbuttongroup.py
@@ -1,8 +1,9 @@
 """AQSButtonGroup module."""
 
-from PyQt5 import QtCore
 from pineboolib.core.utils import logging
 from pineboolib.q3widgets import qbuttongroup
+
+from typing import Any
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 class AQSButtonGroup(qbuttongroup.QButtonGroup):
     """AQSButtonGroup class."""
 
-    def __init__(self, parent: QtCore.QObject, name="", *args):
+    def __init__(self, parent: Any, name="", *args):
         super().__init__(parent)
         if name:
             self.setObjectName(name)
