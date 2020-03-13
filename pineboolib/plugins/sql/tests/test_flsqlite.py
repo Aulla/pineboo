cfdd9a287953fd3247bec8bb2ae88be4a4f66aed
José A. Fernández <aullasistemas@gmail.com>
2020-03-14 00:17:49 +0100

    test para drivers sql

--- pineboolib/plugins/sql/tests/test_flsqlite.py
+++ pineboolib/plugins/sql/tests/test_flsqlite.py
@@ -1,13 +1,19 @@
 """Test_flsqlite module."""
 import unittest
+from pineboolib.loader.main import init_testing, finish_testing
 from pineboolib.plugins.sql import flsqlite
 
 
 class TestFLSqlite(unittest.TestCase):
     """TestFLSqlite Class."""
 
-    def test_basic(self) -> None:
-        """Basics test."""
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Ensure pineboo is initialized for testing."""
+        init_testing()
+
+    def test_basic_1(self) -> None:
+        """Basics test 1."""
 
         driver = flsqlite.FLSQLITE()
 
@@ -23,8 +29,39 @@ class TestFLSqlite(unittest.TestCase):
         self.assertFalse(driver.transaction())
         self.assertFalse(driver.releaseSavePoint(0))
 
-        self.assertEqual(driver.setType("String", 20), " STRING(20)")
-        self.assertEqual(driver.setType("Double"), " DOUBLE")
+        self.assertEqual(driver.setType("String", 20), "VARCHAR(20)")
+        self.assertEqual(driver.setType("sTring", 0), "VARCHAR")
+        self.assertEqual(driver.setType("Double"), "FLOAT")
+        self.assertEqual(driver.setType("Bool"), "BOOLEAN")
+        self.assertEqual(driver.setType("DATE"), "VARCHAR(20)")
+        self.assertEqual(driver.setType("pixmap"), "TEXT")
+        self.assertEqual(driver.setType("bytearray"), "CLOB")
+        self.assertEqual(driver.setType("timestamp"), "DATETIME")
         self.assertEqual(
             driver.process_booleans("'true' AND false AND 'false'"), "1 AND false AND 0"
         )
+
+    def test_basic_2(self) -> None:
+        """Basics test 1."""
+        from pineboolib.application.database import pnsqlcursor
+
+        cursor = pnsqlcursor.PNSqlCursor("fltest")
+        sql = (
+            "CREATE TABLE fltest (id INTEGER PRIMARY KEY,string_field VARCHAR NULL,date_field VARCHAR(20)"
+            + " NULL,time_field VARCHAR(20) NULL,double_field FLOAT NULL,bool_field BOOLEAN NULL"
+            + ",uint_field INTEGER NULL,bloqueo BOOLEAN NOT NULL);CREATE INDEX fltest_pkey ON fltest (id)"
+        )
+        sql2 = (
+            "CREATE TABLE fltest (id INTEGER PRIMARY KEY,string_field VARCHAR NULL,date_field VARCHAR(20)"
+            + " NULL,time_field VARCHAR(20) NULL,double_field FLOAT NULL,bool_field BOOLEAN NULL"
+            + ",uint_field INTEGER NULL,bloqueo BOOLEAN NOT NULL);"
+        )
+        driver = flsqlite.FLSQLITE()
+
+        self.assertEqual(sql, driver.sqlCreateTable(cursor.metadata()))
+        self.assertEqual(sql2, driver.sqlCreateTable(cursor.metadata(), False))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Ensure test clear all data."""
+        finish_testing()
