3422b25bdabc7a950e88d0dfb7477625566ddc2d
José A. Fernández <aullasistemas@gmail.com>
2020-03-26 17:01:31 +0100

    test messagebox.py

---
+++ pineboolib/q3widgets/tests/test_messagebox.py
@@ -0,0 +1,17 @@
+"""Test_messagebox module."""
+
+import unittest
+from pineboolib.q3widgets import messagebox
+
+
+class TestMessageBox(unittest.TestCase):
+    """TestMessageBox Class."""
+
+    def test_buttons(self) -> None:
+        """Test if the buttons exists"""
+        msg_box = messagebox.MessageBox()
+        self.assertTrue(hasattr(msg_box, "Yes"))
+        self.assertTrue(hasattr(msg_box, "No"))
+        self.assertTrue(hasattr(msg_box, "NoButton"))
+        self.assertTrue(hasattr(msg_box, "Ok"))
+        self.assertTrue(hasattr(msg_box, "Cancel"))
