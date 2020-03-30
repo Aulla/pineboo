4f64ed064f91fee2564bc1d72b6abe955305cc5b
José A. Fernández <aullasistemas@gmail.com>
2020-03-30 20:09:50 +0200

    normalizado

--- pineboolib/plugins/sql/flqpsql.py
+++ pineboolib/plugins/sql/flqpsql.py
@@ -622,7 +622,8 @@ class FLQPSQL(pnsqlschema.PNSqlSchema):
             util.setLabelText(util.translate("application", "Creando índices para %s" % table_name))
             util.setProgress(number)
             metadata = self.db_.connManager().manager().metadata(table_name)
-            # if metadata is None:
+            if metadata is None:
+                pass
             #    LOGGER.error("checkSequences: %s metadata not found!", table_name)
 
         util.destroyProgressDialog()
