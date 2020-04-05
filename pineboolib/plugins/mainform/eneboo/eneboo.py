4cf460f81a2da4f9e48c831b8a3e3552173489a5
José A. Fernández <aullasistemas@gmail.com>
2020-04-05 14:03:33 +0200

    normalizado

--- pineboolib/plugins/mainform/eneboo/eneboo.py
+++ pineboolib/plugins/mainform/eneboo/eneboo.py
@@ -1110,7 +1110,7 @@ class MainForm(QtWidgets.QMainWindow):
             actions = node.elementsByTagName("addaction")
         hide_group = True
 
-        actions_widgets = []
+        actions_widgets: List[QtWidgets.QToolBar] = []
         if isinstance(widget, (QtWidgets.QToolBar, QtWidgets.QMenu)):
             actions_widgets = widget.actions()
 
