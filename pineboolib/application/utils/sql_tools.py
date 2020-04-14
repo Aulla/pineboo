8d8a491294b2af31ca1a02bbd0ec0ac0a599faf1
José A. Fernández <aullasistemas@gmail.com>
2020-04-14 12:33:06 +0200

    Dynamycfilter se pasa a sql_tools

--- pineboolib/application/utils/sql_tools.py
+++ pineboolib/application/utils/sql_tools.py
@@ -480,3 +480,76 @@ class SqlInspector(object):
                     if table_name not in self._invalid_tables:
                         self._invalid_tables.append(table_name)
                     # tables_list.remove(table_name)
+
+    # https://ruddra.com/posts/dynamically-constructing-filters-based-on-string-input-using-sqlalchemy/
+
+
+class DynamicFilter(object):
+    def __init__(self, query=None, model_class=None, filter_condition=None):
+        self.query = query
+        self.model_class = model_class
+        self.filter_condition = filter_condition
+
+    def get_query(self):
+        """
+        Returns query with all the objects
+        :return:
+        """
+        if not self.query:
+            self.query = self.session.query(self.model_class)
+        return self.query
+
+    def filter_query(self, query, filter_condition):
+        """
+        Return filtered queryset based on condition.
+        :param query: takes query
+        :param filter_condition: Its a list, ie: [(key,operator,value)]
+        operator list:
+            eq for ==
+            lt for <
+            ge for >=
+            in for in_
+            like for like
+            value could be list or a string
+        :return: queryset
+
+        """
+
+        if query is None:
+            query = self.get_query()
+        # model_class = self.get_model_class()  # returns the query's Model
+        model_class = self.model_class
+        for raw in filter_condition:
+            try:
+                key, op, value = raw
+            except ValueError:
+                raise Exception("Invalid filter: %s" % raw)
+            column = getattr(model_class, key, None)
+            if not column:
+                raise Exception("Invalid filter column: %s" % key)
+            if op == "in":
+                if isinstance(value, list):
+                    filt = column.in_(value)
+                else:
+                    filt = column.in_(value.split(","))
+            else:
+                try:
+                    attr = (
+                        list(filter(lambda e: hasattr(column, e % op), ["%s", "%s_", "__%s__"]))[0]
+                        % op
+                    )
+                except IndexError:
+                    raise Exception("Invalid filter operator: %s" % op)
+                if value == "null":
+                    value = None
+                filt = getattr(column, attr)(value)
+            query = query.filter(filt)
+        return query
+
+    def return_query(self, delete_later: bool = False):
+        if delete_later:
+            return self.filter_query(self.get_query(), self.filter_condition).delete(
+                synchronize_session=False
+            )
+
+        return self.filter_query(self.get_query(), self.filter_condition)
