afae2c72bcf39e9e43633e56e20a9cf989e31af6
José A. Fernández <aullasistemas@gmail.com>
2020-04-30 17:45:03 +0200

    avances spinbox

--- pineboolib/q3widgets/spinbox.py
+++ pineboolib/q3widgets/spinbox.py
@@ -1,34 +1,60 @@
 """Spinbox module."""
-from . import qspinbox
+
+from PyQt5 import QtWidgets
 
 
-class SpinBox(qspinbox.QSpinBox):
+class SpinBox(QtWidgets.QWidget):
     """SpinBox class."""
+
+    _label: "QtWidgets.QLabel"
+    _spin_box: "QtWidgets.QSpinBox"
+    _lay: "QtWidgets.QHBoxLayout"
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self._lay = QtWidgets.QHBoxLayout(self)
+        self._label = QtWidgets.QLabel()
+        self._spin_box = QtWidgets.QSpinBox()
+
+        self._lay.addWidget(self._label)
+        self._lay.addWidget(self._spin_box)
 
     def getMax(self) -> int:
         """Return Maximum."""
-        return super().maximum()
+        return self._spin_box.maximum()
 
     def setMax(self, max: int) -> None:
         """Set Maximum."""
-        super().setMaximum(max)
+        self._spin_box.setMaximum(max)
 
     def getMin(self) -> int:
         """Return Minimum."""
-        return super().minimum()
+        return self._spin_box.minimum()
 
     def setMin(self, min: int) -> None:
         """Set Minimum."""
-        super().setMinimum(min)
+        self._spin_box.setMinimum(min)
 
     def getValue(self) -> int:
         """Return value."""
-        return super().value()
+        return self._spin_box.value()
 
     def setValue(self, value: int) -> None:
         """Set Minimum."""
-        super().setValue(value)
+        self._spin_box.setValue(value)
+
+    def getLabel(self) -> str:
+        """Return label."""
+
+        return self._label.text()
+
+    def setLabel(self, label: str) -> None:
+        """Set label."""
+
+        self._label.setText(label)
 
     maximum: int = property(getMax, setMax)  # type: ignore [assignment] # noqa: F821
     minimum: int = property(getMin, setMin)  # type: ignore [assignment] # noqa: F821
     value: int = property(getValue, setValue)  # type: ignore [assignment] # noqa: F821
+    label: str = property(getLabel, setLabel)  # type: ignore [assignment] # noqa: F821
