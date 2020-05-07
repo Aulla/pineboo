e3e3feb48ae064f61ec34ef91ed4eec32572b88e
José A. Fernández <aullasistemas@gmail.com>
2020-05-07 10:52:31 +0200

    nuevos test orm

--- pineboolib/application/database/orm/tests/test_basemodel.py
+++ pineboolib/application/database/orm/tests/test_basemodel.py
@@ -17,7 +17,7 @@ class TestBaseModel(unittest.TestCase):
     def test_basic_1(self) -> None:
         """Basic test 1."""
 
-        instance = qsa.orm_("flareas")()
+        instance = qsa.orm_("flareas")
         model_class = instance.__class__
 
         session = qsa.session()
@@ -32,7 +32,7 @@ class TestBaseModel(unittest.TestCase):
     def test_basic_2(self) -> None:
         """Basic test 2."""
 
-        instance = qsa.orm_("flareas")()
+        instance = qsa.orm_("flareas")
         model_class = instance.__class__
 
         session = qsa.session()
@@ -47,12 +47,25 @@ class TestBaseModel(unittest.TestCase):
     def test_basic_3(self) -> None:
         """Basic test 3."""
 
-        model_class = qsa.orm_("flareas")
+        model_class = qsa.orm_("flareas").__class__
         session = qsa.session()
 
         area_obj = session.query(model_class).filter(model_class.idarea == "z").first()
         self.assertEqual(area_obj.descripcion, "Descripción M")
         self.assertTrue(area_obj.delete())
+
+    def test_default_value(self) -> None:
+        """Test default values when new instances."""
+
+        obj_ = qsa.orm_("flareas")
+        self.assertEqual(obj_.bloqueo, True)
+
+    def test_metadata(self) -> None:
+        """Test table_metadata."""
+
+        obj_ = qsa.orm_("fltest")
+        meta = obj_.table_metadata()
+        self.assertTrue(meta)
 
     @classmethod
     def tearDownClass(cls) -> None:
