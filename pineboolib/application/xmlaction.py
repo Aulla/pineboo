515b28a00256380474ff31bf07515f7e1eaebd70
José A. Fernández <aullasistemas@gmail.com>
2020-09-15 12:44:00 +0200

    normalizado

--- pineboolib/application/xmlaction.py
+++ pineboolib/application/xmlaction.py
@@ -198,9 +198,9 @@ class XMLAction(struct.ActionStruct):
                     )
             if self._record_widget._form is not None:
                 # FIXME: Borrar bien el formrecord viejo!
-                self._record_widget._form.setParent(
+                self._record_widget._form.setParent(  # type: ignore [call-overload] # noqa: F821
                     None
-                )  # type: ignore [call-overload] # noqa: F821
+                )
                 self._record_widget._form.deleteLater()
                 del self._record_widget._form
                 self._record_widget._form = None
