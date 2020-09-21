c598716909c8a889c494e0fb6ba7f06c357294fd
José A. Fernández <aullasistemas@gmail.com>
2020-09-21 09:06:46 +0200

    normalizado

--- pineboolib/core/test/test_settings.py
+++ pineboolib/core/test/test_settings.py
@@ -1,6 +1,6 @@
 """Test_settings module."""
 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 
 import unittest
 from pineboolib.loader.main import init_testing, finish_testing
@@ -16,19 +16,23 @@ class TestSettings(unittest.TestCase):
         """Ensure pineboo is initialized for testing."""
         init_testing()
 
-    def test_raises(self):
+    def test_raises(self) -> None:
         """Test errors."""
 
         setting = settings.PinebooSettings("TEST")
 
         with self.assertRaises(Exception):
-            sett = setting._value(QtWidgets.QWidget(), None)
+            sett = setting._value(  # type: ignore [arg-type] # noqa : F821
+                QtWidgets.QWidget(), None  # type: ignore [arg-type] # noqa : F821
+            )
 
         with self.assertRaises(TypeError):
-            sett = setting.load_value(QtWidgets.QSizePolicy)
+            sett = setting.load_value(  # type: ignore [arg-type] # noqa : F821
+                QtWidgets.QSizePolicy  # type: ignore [arg-type] # noqa : F821
+            )
 
         with self.assertRaises(TypeError):
-            sett = setting.load_value(QtWidgets.QLabel())
+            sett = setting.load_value(QtWidgets.QLabel())  # type: ignore [arg-type] # noqa : F821
 
     @classmethod
     def tearDownClass(cls) -> None:
