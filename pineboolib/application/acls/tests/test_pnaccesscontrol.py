45cf24225bfe16d5a3d7fc4d6f730ac75da37945
José A. Fernández <aullasistemas@gmail.com>
2020-09-21 10:31:50 +0200

    test pnaccesscontrol

---
+++ pineboolib/application/acls/tests/test_pnaccesscontrol.py
@@ -0,0 +1,33 @@
+"""Test_pnaccesscontrol module."""
+
+from PyQt5 import QtXml
+
+import unittest
+from pineboolib.loader.main import init_testing, finish_testing
+
+
+from .. import pnaccesscontrol
+
+
+class TestPNAccessControl(unittest.TestCase):
+    """TestPNAccessControl Class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Ensure pineboo is initialized for testing."""
+        init_testing()
+
+    def test_missing_lines(self) -> None:
+        """Test missing lines."""
+
+        ac_ = pnaccesscontrol.PNAccessControl()
+        ac_._acos_perms["uno"] = "uno:dos:tres"
+        self.assertFalse(ac_.type())
+        ac_.set(None)
+        ac_.get(None)
+        ac_.set(QtXml.QDomElement())
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Ensure test clear all data."""
+        finish_testing()
