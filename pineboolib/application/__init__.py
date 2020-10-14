2448fe80681160f36355570d1fb55d9b44f60463
José A. Fernández <aullasistemas@gmail.com>
2020-10-14 19:12:07 +0200

    SHOW_TRANSACTIONS_AFTER_ATOMIC por defecto a False

--- pineboolib/application/__init__.py
+++ pineboolib/application/__init__.py
@@ -23,4 +23,4 @@ VIRTUAL_DB: bool = True  # Enable :memory: database on pytest.
 LOG_SQL = False  # Enable sqlalchemy logs.
 USE_WEBSOCKET_CHANNEL = False  # Enable websockets features.
 USE_MISMATCHED_VIEWS = True
-SHOW_TRANSACTIONS_AFTER_ATOMIC = True
+SHOW_TRANSACTIONS_AFTER_ATOMIC = False
