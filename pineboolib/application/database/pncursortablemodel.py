8a4b73a465183a21e649148db5f4117e2be6474f
José A. Fernández <aullasistemas@gmail.com>
2020-10-15 13:01:42 +0200

    fix

--- pineboolib/application/database/pncursortablemodel.py
+++ pineboolib/application/database/pncursortablemodel.py
@@ -1124,14 +1124,10 @@ class ProxyIndex:
 
     def index(self, value: Any) -> int:
         """Return data position."""
-        pos_min = 0
-        pos_max = self._last_current_size - 1
-        while value not in self._cached_data[pos_min:pos_max]:
+
+        while value not in self._cached_data:
             if not self.fetch_more():
                 return -1
-            else:
-                pos_min = pos_max + 1
-                pos_max += 2000
 
         return self._cached_data.index(value)
 
