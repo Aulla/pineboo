2f3f7db995a7d1dd7e713fedda4e21c39d7aa0c7
José A. Fernández <aullasistemas@gmail.com>
2020-10-16 09:03:40 +0200

    is_valid_session al adquirir una session no lanza raise

--- pineboolib/application/database/pnconnection.py
+++ pineboolib/application/database/pnconnection.py
@@ -190,7 +190,7 @@ class PNConnection(QtCore.QObject, iconnection.IConnection):
         session_id = self._get_session_id()
         returned_session = None
 
-        if not self.driver().is_valid_session(session_id):
+        if not self.driver().is_valid_session(session_id, False):
             self.driver().delete_session(session_id)
             new_session = self.driver().session()
             returned_session = new_session[1]
