770729ff45f9d2b996b5855a649099cee268384c
José A. Fernández <aullasistemas@gmail.com>
2020-11-04 19:52:09 +0100

    refactorizando load_script

--- pineboolib/application/load_script.py
+++ pineboolib/application/load_script.py
@@ -5,6 +5,7 @@ from .utils.path import _path
 
 from typing import Optional, TYPE_CHECKING
 
+
 from pineboolib.application.staticloader import pnmodulesstaticloader
 from pineboolib import application
 
@@ -21,6 +22,7 @@ if TYPE_CHECKING:
     from pineboolib.qsa import formdbwidget
     from pineboolib.application import xmlaction
     from sqlalchemy.ext.declarative import api  # noqa: F401
+    from types import ModuleType
 
 LOGGER = logging.get_logger(__name__)
 
@@ -54,25 +56,10 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
                 "%spy" % script_path_qs[:-2] if os.path.exists("%spy" % script_path_qs[:-2]) else ""
             )
 
-        # if script_path_qs:
-        #    LOGGER.info("Found QSA file : %s", script_path_qs)
-        # else:
-        #    LOGGER.info("QSA file not found.")
-
-        # if script_path_py:
-        #    LOGGER.info("Found cached PY file : %s", script_path_py)
-        # else:
-        #    LOGGER.info("PY file not found.")
-
         if application.PROJECT.no_python_cache:
-            # if not script_path_qs:
-            #    LOGGER.warning(
-            #        "load_script: no_python_cache. Using %s. No candidate found for translation",
-            #        script_path_py,
-            #    )
-            # else:
             if script_path_qs:
                 script_path_py = ""
+
         script_path_py_static: str = ""
         script_path_qs_static: str = ""
         static_flag = ""
@@ -81,23 +68,16 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
         if script_path_qs:
             static_flag = "%s/static.xml" % os.path.dirname(script_path_qs)
 
-        mng_modules = application.PROJECT.conn_manager.managerModules()
-        if mng_modules.static_db_info_ and mng_modules.static_db_info_.enabled_:
-            script_path_py_static = pnmodulesstaticloader.PNStaticLoader.content(
-                "%s.py" % script_name, mng_modules.static_db_info_, True
-            )  # Con True solo devuelve el path
+        script_path_py_static = _static_file("%s.py" % script_name)
+        script_path_qs_static = _static_file("%s.qs" % script_name)
 
-            script_path_qs_static = pnmodulesstaticloader.PNStaticLoader.content(
-                "%s.qs" % script_name, mng_modules.static_db_info_, True
-            )  # Con True solo devuelve el path
+        if script_path_qs_static and not script_path_py_static:
 
-            if script_path_qs_static and not script_path_py_static:
+            script_path_py = ""
 
-                script_path_py = ""
-
-            elif script_path_py_static:
-                old_script_path_py = script_path_py
-                script_path_py = script_path_py_static
+        elif script_path_py_static:
+            old_script_path_py = script_path_py
+            script_path_py = script_path_py_static
 
         if script_path_py:
             if os.path.exists(static_flag):
@@ -121,8 +101,7 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
             if not os.path.isfile(script_path_py):
                 raise IOError
             try:
-                loader = machinery.SourceFileLoader(script_name, script_path_py)
-                script_loaded = loader.load_module()  # type: ignore[call-arg] # noqa: F821
+                script_loaded = _load(script_name, script_path_py, False)
             except Exception:
                 LOGGER.exception("ERROR al cargar script PY para la accion %s:", action_._name)
 
@@ -177,10 +156,8 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
 
             if need_parse:
                 if os.path.exists(script_path_py):
-                    # LOGGER.info("Deleting older PY %s", script_path_py)
                     os.remove(script_path_py)
 
-                # if settings.CONFIG.value("application/isDebuggerMode", False):
                 application.PROJECT.message_manager().send(
                     "status_help_msg", "send", ["Convirtiendo script... %s" % script_name]
                 )
@@ -193,16 +170,10 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
                 )
                 if not application.PROJECT.parse_script_list([script_path_qs]):
                     if not os.path.exists(script_path_py):
-                        # LOGGER.error("SCRIPT_NAME: %s", script_name)
-                        # LOGGER.error("QSA_FILE PATH: %s", script_path_qs)
-                        # LOGGER.error("STATIC_LOAD_FILE PATH: %s", script_path_qs_static)
                         raise Exception("The file %s doesn't created\n" % script_path_py)
 
-            # LOGGER.info("Loading script QS %s -> %s", script_name, script_path_py)
             try:
-                loader = machinery.SourceFileLoader(script_name, script_path_py)
-                script_loaded = loader.load_module()  # type: ignore[call-arg] # noqa: F821
-
+                script_loaded = _load(script_name, script_path_py, False)
             except Exception as error:
                 if os.path.exists(script_path_py):
                     os.remove(script_path_py)
@@ -218,8 +189,6 @@ def load_script(script_name: str, action_: "xmlaction.XMLAction") -> "formdbwidg
 
         script_loaded = emptyscript
 
-    # script_loaded.form = script_loaded.FormInternalObj(action_)
-    # LOGGER.info("<-----   END LOADING SCRIPT %s", script_name.upper())
     return script_loaded.FormInternalObj(action_)  # type: ignore[attr-defined] # noqa: F821
 
 
@@ -228,20 +197,11 @@ def load_model(script_name: str, script_path_py: str) -> Optional["type"]:
 
     # script_path_py = _path("%s.py" % script_name, False)
     model_class = None
-
-    mng_modules = application.PROJECT.conn_manager.managerModules()
-    if mng_modules.static_db_info_ and mng_modules.static_db_info_.enabled_:
-
-        script_path_py_static = pnmodulesstaticloader.PNStaticLoader.content(
-            "%s_model.py" % script_name, mng_modules.static_db_info_, True
-        )  # Con True solo devuelve el path
-        if script_path_py_static:
-            script_path_py = script_path_py_static
+    script_path_py = _resolve_script("%s_model.py" % script_name)
 
     if script_path_py:
         class_name = "%s%s" % (script_name[0].upper(), script_name[1:])
-        loader = machinery.SourceFileLoader("model.%s" % class_name, script_path_py)
-        script_loaded = loader.load_module()  # type: ignore[call-arg] # noqa: F821
+        script_loaded = _load("model.%s" % class_name, script_path_py)
         module_class = getattr(script_loaded, class_name, None)
         if module_class:
             module_class.__metaclass__ = "Base"
@@ -256,57 +216,68 @@ def load_model(script_name: str, script_path_py: str) -> Optional["type"]:
     return model_class
 
 
-def load_class(script_name):
+def load_class(script_name: str):
     """Return class from path."""
 
-    script_path_py = _path("%s.py" % script_name, False)
     class_loaded = None
-
-    mng_modules = application.PROJECT.conn_manager.managerModules()
-    if mng_modules.static_db_info_ and mng_modules.static_db_info_.enabled_:
-        script_path_py_static = pnmodulesstaticloader.PNStaticLoader.content(
-            "%s.py" % script_name, mng_modules.static_db_info_, True
-        )  # Con True solo devuelve el path
-        if script_path_py_static:
-            script_path_py = script_path_py_static
+    script_path_py = _resolve_script("%s.py" % script_name)
 
     if script_path_py:
+        class_name = "%s%s" % (script_name[0].upper(), script_name[1:])
+        script_loaded = _load(script_name, script_path_py)
         try:
-            loader = machinery.SourceFileLoader(script_name, script_path_py)
-            script_loaded = loader.load_module()  # type: ignore[call-arg] # noqa: F821
-            class_loaded = getattr(
-                script_loaded, "%s%s" % (script_name[0].upper(), script_name[1:]), None
-            )
+            class_loaded = getattr(script_loaded, class_name, None)
         except Exception as error:
             LOGGER.error("Error loading class %s: %s", script_name, str(error))
 
     return class_loaded
 
 
-def load_module(script_name):
+def load_module(script_name: str) -> Optional["ModuleType"]:
     """Return class from path."""
 
-    script_path_py = _path(script_name, False)
-    script_loaded = None
+    script_path_py = _resolve_script(script_name)
+    if script_path_py:
+        return _load(script_name[:-3], script_path_py)
+
+    return None
+
+
+def _resolve_script(file_name) -> str:
+    """Resolve script."""
+
+    static = _static_file(file_name)
+    result = static if static else _path(file_name, False)
+    return result or ""
+
+
+def _load(  # type: ignore [return] # noqa: F821
+    module_name: str, script_name: str, capture_error: bool = True
+) -> "ModuleType":
+    """Load modules."""
+
+    try:
+        loader = machinery.SourceFileLoader(module_name, script_name)
+        return loader.load_module()  # type: ignore[call-arg] # noqa: F821
+
+    except Exception as error:
+        if capture_error:
+            LOGGER.error("Error loading module %s: %s", script_name, str(error), stack_info=True)
+        else:
+            raise error
+
+
+def _static_file(file_name: str) -> str:
+    """Return static file."""
+
+    result = ""
     mng_modules = application.PROJECT.conn_manager.managerModules()
     if mng_modules.static_db_info_ and mng_modules.static_db_info_.enabled_:
-        script_path_py_static = pnmodulesstaticloader.PNStaticLoader.content(
-            "%s.py" % script_name, mng_modules.static_db_info_, True
+        result = pnmodulesstaticloader.PNStaticLoader.content(
+            file_name, mng_modules.static_db_info_, True
         )  # Con True solo devuelve el path
-        if script_path_py_static:
-            script_path_py = script_path_py_static
 
-    if script_path_py:
-        try:
-            loader = machinery.SourceFileLoader(script_name[:-3], script_path_py)
-            script_loaded = loader.load_module()  # type: ignore[call-arg] # noqa: F821
-            # class_loaded = getattr(
-            #    script_loaded, "%s%s" % (script_name[0].upper(), script_name[1:]), None
-            # )
-        except Exception as error:
-            LOGGER.error("Error loading module %s: %s", script_name, str(error), stack_info=True)
-
-    return script_loaded
+    return result
 
 
 def get_static_flag(database_path: str, static_path: str) -> "ET.Element":
