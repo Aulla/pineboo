a78a7c91165cbc7548c34a30e56c825c76d6d22f
José A. Fernández <aullasistemas@gmail.com>
2020-11-10 09:53:36 +0100

    actualziando test

--- pineboolib/plugins/mainform/eneboo/tests/test_eneboo.py
+++ pineboolib/plugins/mainform/eneboo/tests/test_eneboo.py
@@ -94,8 +94,8 @@ class TestEnebooGUI(unittest.TestCase):
         application.PROJECT.main_window.initFromWidget(application.PROJECT.main_window)
         application.PROJECT.main_window.triggerAction("triggered():shConsole():clientes")
 
-        self.assertTrue(
-            application.PROJECT.aq_app.getTabWidgetPages(application.PROJECT.main_window)
+        self.assertFalse(
+            application.PROJECT.aq_app.getTabWidgetPages("formRecordclientes", "prueba")
         )
 
         application.PROJECT.aq_app.checkAndFixTransactionLevel("prueba")
