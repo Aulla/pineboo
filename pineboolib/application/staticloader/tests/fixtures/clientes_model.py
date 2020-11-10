6174e72c6d8f59194062b4eaea1ca47c66ff6ccb
José A. Fernández <aullasistemas@gmail.com>
2020-11-10 09:59:52 +0100

    normalizado

--- pineboolib/application/staticloader/tests/fixtures/clientes_model.py
+++ pineboolib/application/staticloader/tests/fixtures/clientes_model.py
@@ -3,20 +3,17 @@
 """Clientes_model module."""
 
 from pineboolib.application.database.orm import basemodel
-from pineboolib.qsa import qsa
-
-import sqlalchemy
 
 
 # @class_declaration Oficial
 class Oficial(basemodel.BaseModel):  # type: ignore [misc] # noqa: F821
-    """ Oficial class."""
+    """Oficial class."""
 
     __tablename__ = "clientes"
 
 
 # @class_declaration Clientes
 class Clientes(Oficial):  # type: ignore [misc] # noqa: F821
-    """ Clientes class."""
+    """Clientes class."""
 
     pass
