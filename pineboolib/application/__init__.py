50878290e1c7bb2436d0135beca095eecc4a78e7
José A. Fernández <aullasistemas@gmail.com>
2020-11-14 09:32:17 +0100

    0.76.10.1

--- pineboolib/application/__init__.py
+++ pineboolib/application/__init__.py
@@ -13,7 +13,7 @@ ATOMIC_LIST: Dict[int, List[str]] = {}
 SERIALIZE_LIST: Dict[int, List[str]] = {}
 FILE_CLASSES: Dict[str, str] = {}
 
-PINEBOO_VER = "0.76.10"
+PINEBOO_VER = "0.76.10.1"
 
 SHOW_CURSOR_EVENTS: bool = False  # Enable show pnsqlcursor actions debug.
 SHOW_CLOSED_CONNECTION_WARNING: bool = False  # Enable show debug when connection is closed.
