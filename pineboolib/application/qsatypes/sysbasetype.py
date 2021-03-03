567751efa4a59302db3daf29fd03ebaa8c2ca74b
José A. Fernández <aullasistemas@gmail.com>
2021-03-03 07:57:08 +0100

    ajuste mezcla

--- pineboolib/application/qsatypes/sysbasetype.py
+++ pineboolib/application/qsatypes/sysbasetype.py
@@ -232,11 +232,7 @@ class SysBaseType(object):
         """Get Session ID."""
 
         # FIXME: Code copied from flapplication.aqApp
-<<<<<<< HEAD
-        return application.ID_SESSION.toString(QtCore.Qt.DateFormat.ISODate)
-=======
         return application.ID_SESSION
->>>>>>> f9cb9004fb767f929cf126289b1b006c3a809289
 
     @classmethod
     def reportChanges(cls, changes: Dict[str, str] = {}):
