eff75d9c8478f3fd492554b9cd86618c012e55b0
José A. Fernández <aullasistemas@gmail.com>
2021-04-12 10:28:33 +0200

    ajustes ui

--- pineboolib/system_module/scripts/flfiles.py
+++ pineboolib/system_module/scripts/flfiles.py
@@ -9,10 +9,16 @@ class FormInternalObj(qsa.FormDBWidget):
     def init(self) -> None:
         """Init function."""
         fdb_contenido = self.child("contenido")
+        fdb_nombre = self.child("nombre")
+        fdb_sha = self.child("sha")
+
         if fdb_contenido is None:
             raise Exception("contenido control not found!.")
 
         fdb_contenido.setText(self.cursor().valueBuffer("contenido"))
+        fdb_nombre.setValue(self.cursor().valueBuffer("nombre"))
+        fdb_sha.setValue(self.cursor().valueBuffer("sha"))
+
         edit_button = self.child("botonEditar")
         edit_pb_xml = self.child("pbXMLEditor")
 
