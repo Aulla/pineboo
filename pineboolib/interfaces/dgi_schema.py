d10364f5d3e071a65ade18e72fe94083a0b3bd5a
José A. Fernández <aullasistemas@gmail.com>
2021-09-01 10:59:26 +0200

    repaso pylint de interfaces

--- pineboolib/interfaces/dgi_schema.py
+++ pineboolib/interfaces/dgi_schema.py
@@ -3,4 +3,4 @@ Import dgi_schema to interface package.
 """
 from pineboolib.plugins.dgi import dgi_schema as dgi_schema_module  # pragma: no cover
 
-dgi_schema = dgi_schema_module.DgiSchema  # pragma: no cover
+dgi_schema = dgi_schema_module.DgiSchema  # pragma: no cover # pylint: disable=invalid-name
