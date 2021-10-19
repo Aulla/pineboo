6a10fa7c16d0313d68fb334b2f6ab7f7a3dc0e62
José A. Fernández <aullasistemas@gmail.com>
2021-10-20 00:36:28 +0200

    fix cunado el alias contiene '

--- pineboolib/application/parsers/parser_mtd/pnmtdparser.py
+++ pineboolib/application/parsers/parser_mtd/pnmtdparser.py
@@ -150,7 +150,7 @@ def generate_field_metadata(field: "pnfieldmetadata.PNFieldMetaData") -> List[st
 
     # ALIAS
     if field.alias():
-        field_data.append("'alias' : '%s'" % field.alias())
+        field_data.append("'alias' : '%s'" % field.alias().replace("'", '"'))
 
     # PK
     if field.isPrimaryKey():
