c058efc83c9ab137e39411465df02f28c94962eb
José A. Fernández <aullasistemas@gmail.com>
2021-11-08 07:24:44 +0100

    normalizado

--- pineboolib/application/utils/sql_tools.py
+++ pineboolib/application/utils/sql_tools.py
@@ -658,7 +658,7 @@ class SqlInspector(object):
                     # tables_list.remove(table_name)
 
     def suspected_injection(self) -> bool:
-        """Returns if the query contains suspicion of sql injection-"""
+        """Return if the query contains suspicion of sql injection."""
 
         return False if self._suspected_injection is None else True
 
@@ -692,4 +692,3 @@ class SqlInspector(object):
 
         if infected:
             self._suspected_injection = infected
-
