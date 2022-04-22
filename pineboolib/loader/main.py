22070dc5ecd30004700b39a37c25a2dfcae04408
José A. Fernández <aullasistemas@gmail.com>
2022-04-22 15:58:38 +0200

    se puede especificar un valor en init_testing para el log

--- pineboolib/loader/main.py
+++ pineboolib/loader/main.py
@@ -253,7 +253,7 @@ def setup_gui(app: "QtWidgets.QApplication") -> None:
     app.setFont(font)
 
 
-def init_testing() -> None:
+def init_testing(level: int = 1000) -> None:
     """Initialize Pineboo for testing purposes."""
     settings.CONFIG.set_value("application/dbadmin_enabled", True)
 
@@ -271,7 +271,7 @@ def init_testing() -> None:
 
         LOGGER.info("PINEBOO TESTING %s.", application.PINEBOO_VER)
         # application.PROJECT.load_version()
-        application.PROJECT.setDebugLevel(1000)
+        application.PROJECT.setDebugLevel(level)
         application.PROJECT.set_app(qapp)
 
         dgi = dgi_module.load_dgi("qt", None)
