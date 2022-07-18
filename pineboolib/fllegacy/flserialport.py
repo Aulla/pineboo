ace4f64f7767b780fea671b6697b03a424defff4
José A. Fernández <aullasistemas@gmail.com>
2022-07-18 11:45:06 +0200

    normalizado

--- pineboolib/fllegacy/flserialport.py
+++ pineboolib/fllegacy/flserialport.py
@@ -4,66 +4,66 @@ from PyQt6 import QtCore , QtSerialPort  # type: ignore[import]
 from pineboolib.core import decorators
 
 
-class BaudRateType(QtSerialPort.QSerialPort.BaudRate):
+class BaudRateType(object):
     """BaudRateType class."""
 
-    # BAUD50: int = -1
-    # BAUD75: int = -1
-    # BAUD110: int = -1
-    # BAUD134: int = -1
-    # BAUD150: int = -1
-    # BAUD200: int = -1
-    # BAUD300: int = -1
-    # BAUD600: int = -1
-    # BAUD1200: int = 1200
-    # BAUD1800: int = -1
-    # BAUD2400: int = 2400
-    # BAUD4800: int = 4800
-    # BAUD9600: int = 9600
-    # BAUD14400: int = -1
-    # BAUD19200: int = 19200
-    # BAUD38400: int = 38400
-    # BAUD56000: int = -1
-    # BAUD57600: int = 57600
-    # BAUD76800: int = -1
-    # BAUD115200: int = 115200
-    # BAUD128000: int = -1
-    # BAUD256000: int = -1
+    BAUD50: int = -1
+    BAUD75: int = -1
+    BAUD110: int = -1
+    BAUD134: int = -1
+    BAUD150: int = -1
+    BAUD200: int = -1
+    BAUD300: int = -1
+    BAUD600: int = -1
+    BAUD1200: int = 1200
+    BAUD1800: int = -1
+    BAUD2400: int = 2400
+    BAUD4800: int = 4800
+    BAUD9600: int = 9600
+    BAUD14400: int = -1
+    BAUD19200: int = 19200
+    BAUD38400: int = 38400
+    BAUD56000: int = -1
+    BAUD57600: int = 57600
+    BAUD76800: int = -1
+    BAUD115200: int = 115200
+    BAUD128000: int = -1
+    BAUD256000: int = -1
 
 
-class DataBitsType(QtSerialPort.QSerialPort.DataBits):
+class DataBitsType(object):
     """DataBitsType class."""
 
-    # DATA_5: int = 5
-    # DATA_6: int = 6
-    # DATA_7: int = 7
-    # DATA_8: int = 8
+    DATA_5: int = 5
+    DATA_6: int = 6
+    DATA_7: int = 7
+    DATA_8: int = 8
 
 
-class ParityType(QtSerialPort.QSerialPort.Parity):
+class ParityType(object):
     """ParityType class."""
 
-    # PAR_NONE: int = 0
-    # PAR_EVEN: int = 2
-    # PAR_ODD: int = 3
-    # PAR_SPACE: int = 4
-    # PAR_MARK: int = 5
+    PAR_NONE: int = 0
+    PAR_EVEN: int = 2
+    PAR_ODD: int = 3
+    PAR_SPACE: int = 4
+    PAR_MARK: int = 5
 
 
-class StopBitType(QtSerialPort.QSerialPort.StopBits):
+class StopBitType(object):
     """StopBitType class."""
 
-    # STOP_1: int = 1
-    # STOP_1_5: int = 3
-    # STOP_2: int = 2
+    STOP_1: int = 1
+    STOP_1_5: int = 3
+    STOP_2: int = 2
 
 
-class FlowType(QtSerialPort.QSerialPort.FlowControl):
+class FlowType(object):
     """FlowType class."""
 
-    # FLOW_OFF: int = 0
-    # FLOW_HARDWARE: int = 1
-    # FLOW_XONXOFF: int = 2
+    FLOW_OFF: int = 0
+    FLOW_HARDWARE: int = 1
+    FLOW_XONXOFF: int = 2
 
 
 class FLSerialPort(QtCore.QObject, BaudRateType, DataBitsType, ParityType, StopBitType, FlowType):
