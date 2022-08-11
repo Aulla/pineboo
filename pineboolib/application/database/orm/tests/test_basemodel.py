46f979efdc3037ecebd76bd86e268572988d7018
José A. Fernández <aullasistemas@gmail.com>
2022-08-11 14:15:21 +0200

    fix test

--- pineboolib/application/database/orm/tests/test_basemodel.py
+++ pineboolib/application/database/orm/tests/test_basemodel.py
@@ -264,10 +264,10 @@ class TestBaseModel(unittest.TestCase):
         """Test counter."""
         qsa.thread_session_new()
         obj_class = qsa.orm_("fltest3")
-        obj_ = obj_class()
+        obj_ = obj_class(counter=True)
         self.assertEqual(obj_.counter_field, "000001")
         obj_class = qsa.orm_("fltest3")
-        obj2_ = obj_class(counter=False)
+        obj2_ = obj_class()
         self.assertEqual(obj2_.counter_field, None)
 
     def test_z_delete(self) -> None:
