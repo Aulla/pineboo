3f119e01066a53f3808088ded9e70d3fa4900ddd
José A. Fernández <aullasistemas@gmail.com>
2023-05-10 11:36:35 +0200

    normalizado

--- pineboolib/fllegacy/flutil.py
+++ pineboolib/fllegacy/flutil.py
@@ -266,7 +266,6 @@ class FLUtil(object):
             buffer = "Sólo hay capacidad hasta mil millones"
 
         elif num < 1000000:
-
             buffer = cls.centenamillar(int(num))
 
         else:
@@ -305,15 +304,12 @@ class FLUtil(object):
             res = "%s %s" % (cls.enLetra(entero), currency)
 
             if decimal > 0:
-
                 res += " con %s céntimos" % cls.enLetra(decimal)
 
         if entero <= 0 and decimal > 0:
-
             res = "%s céntimos" % cls.enLetra(decimal)
 
         if num < 0.00:
-
             res = "menos %s" % res
 
         return res.upper()
@@ -1186,7 +1182,7 @@ class FLUtil(object):
         @param field_name. Field Name.
         @param table_name. Name of the table containing the field.
         @param conn_name. Name of the connection to use.
-        @return Alias ​​of the specified field.
+        @return Alias of the specified field.
         """
         conn = application.PROJECT.conn_manager.useConn(conn_name)
         metadata = conn.connManager().manager().metadata(table_name)
@@ -1202,7 +1198,7 @@ class FLUtil(object):
 
         @param table_name. Table name
         @param conn_name. Name of the connection to use
-        @return Alias ​​of the specified table
+        @return Alias of the specified table
         """
         conn = application.PROJECT.conn_manager.useConn(conn_name)
         metadata = conn.connManager().manager().metadata(table_name)
@@ -1219,7 +1215,7 @@ class FLUtil(object):
         @param alias. Field Name
         @param table_name. Name of the table containing the field
         @param conn_name. Name of the connection to use
-        @return Alias ​​of the specified field
+        @return Alias of the specified field
         """
         conn = application.PROJECT.conn_manager.useConn(conn_name)
         metadata = conn.connManager().manager().metadata(table_name)
