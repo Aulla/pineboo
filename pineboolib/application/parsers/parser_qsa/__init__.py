7808290e7448e98ca2f4602511dc4525fdfe72b0
José A. Fernández <aullasistemas@gmail.com>
2023-06-23 11:08:15 +0200

    parser qsa 1.10

--- pineboolib/application/parsers/parser_qsa/__init__.py
+++ pineboolib/application/parsers/parser_qsa/__init__.py
@@ -3,6 +3,6 @@ Parse QS files and write Python files.
 """
 
 USE_THREADS: bool = True
-PARSER_QSA_VERSION: str = "1.9"
+PARSER_QSA_VERSION: str = "1.10"
 IGNORE_NO_PYTHON_TAGS: bool = False
 STRICT_MODE: bool = False
