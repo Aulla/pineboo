9bad145f6e6b99e5683fac593d1e0ba58c072c6b
José A. Fernández <aullasistemas@gmail.com>
2023-10-04 12:09:06 +0200

    Revert "mejoras next_counter2"
    This reverts commit 474f86cc320924b0361475c328cebedacbb17054.

--- pineboolib/application/database/utils.py
+++ pineboolib/application/database/utils.py
@@ -108,16 +108,14 @@ def _next_counter2(
     if not qry.exec_():
         return None
 
-    _value: str = qry.value(0) if qry.last() else "0"
-    _serie: str = "".join(digit for digit in _value if not digit.isdigit())
-    _len_serie = len(_serie)
-    _len_numero = _len - _len_serie
-    print("***", _value, _serie, _len_serie, _len_numero)
-    _numero: int = int(_value[_len_serie:]) + 1
+    _numero: int = int(qry.value(0)) if qry.last() else 0
+
+    _numero += 1
 
     if type_ == "string":
-        _numero = str(_numero).rjust(_len_numero, "0")
-        return "%s%s" % (_serie, _numero)
+        _cadena = str(_numero)
+
+        return _cadena.rjust(_len, "0") if len(_cadena) < _len else _cadena
 
     elif type_ == "double":
         return _numero
